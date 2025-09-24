1b6b274a4e7eb2bf8ff30705b4a143f6c3a81728
Tecnodipli <tecno@dipli.ai>
2025-09-24 10:14:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -23,12 +23,14 @@ ALLOWED_ORIGINS = [
     "https://dipli.ai",
     "https://isagarcivill09.wixsite.com/turop",
     "https://isagarcivill09.wixsite.com/turop/tienda",
-    "https://isagarcivill09-wixsite-com.filesusr.com"
+    "https://isagarcivill09-wixsite-com.filesusr.com",
+    "https://www.dipli.ai/preparaci%C3%B3n",
+    "https://www-dipli-ai.filesusr.com"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOWED_ORIGINS,
+    allow_origins=ALLOWED_ORIGINS,   # ✅ ahora sí aplica tu lista
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
